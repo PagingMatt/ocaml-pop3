f398fae71fc7c5b50e2da1199647462a6ed52f8b
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-14 23:09:25 +0100

    Adds command types

---
+++ lib/Command.ml
@@ -0,0 +1,13 @@
+type t =
+  | Apop of string * string
+  | Dele of int
+  | List of int option
+  | Noop
+  | Quit
+  | Retr of int
+  | Rset
+  | Stat
+  | Top of int * int
+  | Uidl of int option
+  | User of string
+  | Pass of string

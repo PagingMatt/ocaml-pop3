ca97e32079ef7562ff155117c595c146f054ed60
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-14 23:51:45 +0100

    Adds type and doc for Command

---
+++ lib/Command.mli
@@ -0,0 +1,14 @@
+(** All minimal and optional POP3 commands. *)
+type t =
+  | Apop of string * string
+  | Dele of int
+  | List of int option
+  | Noop
+  | Quit
+  | Retr of int
+  | Rset
+  | Stat
+  | Top of int * int
+  | Uidl of int option
+  | User of string
+  | Pass of string

4423f5d6ed2f44b6002ad36bd973b5cf432fb304
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-16 23:18:54 +0100

    Adds no-op server module

---
+++ lib/Server.ml
@@ -0,0 +1,12 @@
+open Conduit_lwt_unix
+open Lwt.Infix
+
+let callback _flow _input_channel output_channel =
+  Lwt_io.write output_channel "+OK POP3 server disconnecting"
+  (* TODO: implement state machine representing server. *)
+  >|= ignore
+
+let start (stop:unit Lwt.t) =
+  let ctx = default_ctx in
+  let mode = `TCP (`Port 110) in
+  Conduit_lwt_unix.serve ~stop ~ctx ~mode callback
---
+++ lib/Server.mli
@@ -0,0 +1,10 @@
+(** Starts a TCP server listening on port 110 to serve POP3 client connections.
+    
+    There is no limit to the number of concurrent connections and connections
+    do not timeout.
+
+    The parameter passed to the function is a lightweight thread which will
+    kill the server should it become determined.
+
+    @return unit lightweight thread. *)
+val start : unit Lwt.t -> unit Lwt.t

09f7a8ea3e2d8f74a29062ea28618c1eaf965748
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-20 20:43:04 +0100

    Fixes server sig

--- lib/Server.mli
+++ lib/Server.mli
@@ -1,7 +1,7 @@
 (** POP3 server. *)
 
 (** Starts a TCP server listening on port 110 to serve POP3 client connections.
-    
+
     There is no limit to the number of concurrent connections and connections
     do not timeout.
 

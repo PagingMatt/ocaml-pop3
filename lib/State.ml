9dde07a0ec67ec775e8d71f5a9839d32d003e9c4
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-20 20:40:36 +0100

    Fixes build

--- lib/State.ml
+++ lib/State.ml
@@ -1,52 +1,46 @@
 open Store
 open Unix
 
-type authorization_state = | Banner of tm | Mailbox of tm * string | Quit
-type transaction_state   = string
-type update_state        = string
-
-type 'a command_result = {
-  state : 'a;
-  reply : Reply.t;
-  next : bool;
-}
-
 module type Banner = sig
-  val get_time : unit -> tm
+  val time : unit -> tm
 end
 
 module GmTimeBanner : Banner = struct
-  let get_time () =
+  let time () =
     time () |> gmtime
 end
 
 module State (B : Banner) (S : Store) : sig
-  type t =
+  type pop3_session_state =
     | Disconnected
-    | Authorization of authorization_state
-    | Transaction   of transaction_state
-    | Update        of update_state
+    | Authorization of string option
+    | Transaction   of string
+    | Update        of string
+
+  type t = pop3_session_state * tm
 
   val start : unit -> t
 
   val f : t -> Command.t -> (t * Reply.t) Lwt.t
 end = struct
-  type t =
+  type pop3_session_state =
     | Disconnected
-    | Authorization of authorization_state
-    | Transaction   of transaction_state
-    | Update        of update_state
+    | Authorization of string option
+    | Transaction   of string
+    | Update        of string
 
-  let start () = Authorization (Banner (B.get_time ()))
+  type t = pop3_session_state * tm
 
-  let f state _cmd =
+  let start () = (Authorization None, B.time ())
+
+  let f (state, banner_time) _cmd =
     match state with
     | Disconnected ->
-      Lwt.return (Disconnected, Reply.internal_error)
-    | Authorization _auth_state ->
-      Lwt.return (Disconnected, Reply.internal_error)
-    | Transaction _trans_state ->
-      Lwt.return (Disconnected, Reply.internal_error)
-    | Update _update_state ->
-      Lwt.return (Disconnected, Reply.internal_error)
+      Lwt.return ((Disconnected, banner_time), Reply.internal_error)
+    | Authorization _ ->
+      Lwt.return ((Disconnected, banner_time), Reply.internal_error)
+    | Transaction _ ->
+      Lwt.return ((Disconnected, banner_time), Reply.internal_error)
+    | Update _ ->
+      Lwt.return ((Disconnected, banner_time), Reply.internal_error)
 end
--- lib/State.mli
+++ lib/State.mli
@@ -3,49 +3,9 @@
 open Store
 open Unix
 
-(** The inner state of the POP3 'Authorization' state.
-
-    The [string option] value is the mailbox identifier if USER+PASS
-    authorization is being used and the previously issued command was a
-    successful USER command. *)
-type authorization_state =
-  | Banner of tm
-  (** Initial state and that returned to on unsuccessful authorization attempts.
-
-      The [tm] value is the 'banner time' of the connection which can be used
-      for APOP authorization. *)
-  | Mailbox of tm * string
-  (** Intermediate state after successful USER command.
-
-      The [tm] value is the 'banner time' of the connection which can be used
-      for APOP authorization should the subsequent PASS command fail and the
-      initial state is returned to. *)
-  | Quit
-  (** Abort state when QUIT command is issued. *)
-
-(** The inner state of the POP3 'Transaction' state.
-
-    The [string] value is the mailbox identifier that has been authorized
-    previously in the session. *)
-type transaction_state = string
-
-(** The inner state of the POP3 'Update' state.
-
-    The [string] value is the mailbox identifier that has been authorized
-    previously in the session. *)
-type update_state = string
-
-(** Result of handling a command. This records state, reply to send to client
-    and whether to move into next session state. *)
-type 'a command_result = {
-  state : 'a;
-  reply : Reply.t;
-  next : bool;
-}
-
 (** Module type for generating 'banner time' when connections are created. *)
 module type Banner = sig
-  val get_time : unit -> tm
+  val time : unit -> tm
 end
 
 (** Implementation of [Banner] module type for [gmtime]. *)
@@ -56,16 +16,18 @@ module GmTimeBanner : Banner
     and mail drops. *)
 module State (B : Banner) (S : Store) : sig
   (** POP3 session states as defined in RFC 1939. *)
-  type t =
+  type pop3_session_state =
     | Disconnected
     (** [Disconnected] represents the termination of the POP3 session. *)
-    | Authorization of authorization_state
+    | Authorization of string option
     (** [Authorization] represents the initial state of the POP3 session. *)
-    | Transaction of transaction_state
+    | Transaction of string
     (** [Authorization] represents the intermediate state of the POP3
         session. *)
-    | Update of update_state
+    | Update of string
     (** [Update] represents the final state of the POP3 session. *)
+
+  type t = pop3_session_state * tm
 
   (** Start a new POP3 session with 'banner time' from [B] whenever [start] is
       evaluated.

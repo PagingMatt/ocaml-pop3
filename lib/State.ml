906417f2e8dcb15ec28be7473c94d8fc0e40e23e
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-20 23:27:13 +0100

    Implements PASS

--- lib/State.ml
+++ lib/State.ml
@@ -34,34 +34,48 @@ module BackingStoreState (B : Banner) (S : Store) : State = struct
     S.init p
     >|= fun store -> (Authorization None, B.time (), store)
 
+  let auth_fail banner_time store =
+    ((Authorization None, banner_time, store), Reply.err None)
+
+  let auth_result store banner_time mailbox success =
+    if success then
+      ((Transaction mailbox, banner_time, store), Reply.ok (Some mailbox) [])
+    else auth_fail banner_time store
+
   let auth_apop store banner_time mailbox digest =
-    S.apop_of_mailbox store banner_time "" mailbox
-      >|= fun digest_option ->
-        match digest_option with
-        | None -> ((Authorization None, banner_time, store), Reply.err None)
-        | Some digest' ->
-          if digest = digest' then
-          ((Transaction mailbox, banner_time, store),
-            Reply.ok (Some mailbox) []) else
-          ((Authorization None, banner_time, store),
-            Reply.err None)
+    S.apop_of_mailbox store banner_time "" (* TODO: Add hostname to state *) mailbox
+    >|= fun digest_option ->
+      match digest_option with
+      | None -> auth_fail banner_time store
+      | Some digest' -> auth_result store banner_time mailbox (digest = digest')
+
+  let auth_pass store banner_time mailbox secret =
+    S.secret_of_mailbox store mailbox
+    >|= fun secret_option ->
+      match secret_option with
+      | None -> auth_fail banner_time store
+      | Some secret' -> auth_result store banner_time mailbox (secret = secret')
 
   let auth_user store banner_time mailbox =
     Lwt.return
       ((Authorization (Some mailbox), banner_time, store),
         Reply.ok (Some mailbox) [])
 
-  let f_auth_a banner_time store cmd =
+  let f_auth_none banner_time store cmd =
     match cmd with
     | User mailbox -> auth_user store banner_time mailbox
     | Apop (mailbox, digest) -> auth_apop store banner_time mailbox digest
-    | _ -> Lwt.return ((Authorization None, banner_time, store), Reply.err None)
+    | _ -> Lwt.return (auth_fail banner_time store)
+
+  let f_auth_some banner_time store mailbox cmd =
+    match cmd with
+    | Pass secret -> auth_pass store banner_time mailbox secret
+    | _ -> Lwt.return (auth_fail banner_time store)
 
   let f (state, banner_time, store) cmd =
     match state with
-    | Authorization None -> f_auth_a banner_time store cmd
-    | Authorization (Some _mailbox) ->
-      Lwt.return ((Disconnected, banner_time, store), Reply.internal_error)
+    | Authorization None -> f_auth_none banner_time store cmd
+    | Authorization (Some mailbox) -> f_auth_some banner_time store mailbox cmd
     | Disconnected ->
       Lwt.return ((Disconnected, banner_time, store), Reply.internal_error)
     | Transaction _ ->

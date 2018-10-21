ae0e5fa9864f2347c25660e1efdf5d8dc1114fc8
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-21 20:44:46 +0100

    Fixes greeting unit test

--- lib_tests/Reply.ml
+++ lib_tests/Reply.ml
@@ -51,10 +51,12 @@ let lines_of_t_ok_some_lines () =
   | _ -> Alcotest.fail "Unexpected line pattern."
 
 let greeting_is_ok_some_greeting () =
-  match Common.greeting |> lines_of_t with
+  let t = Unix.gmtime 1514764801. in
+  let h = "localhost" in
+  match Common.greeting h t |> lines_of_t with
   | l::[] ->
-    Alcotest.(check string) "Checking 'greeting' is 'OK (Some greetings from OCaml POP3)'."
-      "+OK greetings from OCaml POP3" l
+    Alcotest.(check string) "Checking 'greeting' is 'OK (Some 'server ready <1514764801.0@localhost>')'."
+      "+OK server ready <1514764801.000000@localhost>" l
   | _ -> Alcotest.fail "Unexpected line pattern."
 
 let internal_error_is_error_none () =
@@ -65,12 +67,12 @@ let internal_error_is_error_none () =
   | _ -> Alcotest.fail "Unexpected line pattern."
 
 let unit_tests = [
-  ("Checking serialization of 'Error None'"                            , `Quick, lines_of_t_error_none       );
-  ("Checking serialization of 'Error (Some 'A')'"                      , `Quick, lines_of_t_error_some       );
-  ("Checking serialization of 'Ok (None, [])'"                         , `Quick, lines_of_t_ok_none_empty    );
-  ("Checking serialization of 'Ok (None, ['A'; 'B'])'"                 , `Quick, lines_of_t_ok_none_lines    );
-  ("Checking serialization of 'Ok (Some 'A', [])'"                     , `Quick, lines_of_t_ok_some_empty    );
-  ("Checking serialization of 'Ok (Some 'A', ['B'; 'C'])'"             , `Quick, lines_of_t_ok_some_lines    );
-  ("Checking 'Reply.greeting' is 'OK (Some greetings from OCaml POP3)'", `Quick, greeting_is_ok_some_greeting);
-  ("Checking 'Reply.internal_error' is 'Error None'"                   , `Quick, internal_error_is_error_none);
+  ("Checking serialization of 'Error None'"                                          , `Quick, lines_of_t_error_none       );
+  ("Checking serialization of 'Error (Some 'A')'"                                    , `Quick, lines_of_t_error_some       );
+  ("Checking serialization of 'Ok (None, [])'"                                       , `Quick, lines_of_t_ok_none_empty    );
+  ("Checking serialization of 'Ok (None, ['A'; 'B'])'"                               , `Quick, lines_of_t_ok_none_lines    );
+  ("Checking serialization of 'Ok (Some 'A', [])'"                                   , `Quick, lines_of_t_ok_some_empty    );
+  ("Checking serialization of 'Ok (Some 'A', ['B'; 'C'])'"                           , `Quick, lines_of_t_ok_some_lines    );
+  ("Checking 'Reply.greeting' is 'OK (Some 'server ready <1514764801.0@localhost>')'", `Quick, greeting_is_ok_some_greeting);
+  ("Checking 'Reply.internal_error' is 'Error None'"                                 , `Quick, internal_error_is_error_none);
 ]

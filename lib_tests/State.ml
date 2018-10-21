a04d9b0ae5bc95e9c2a7cb5e09fc80d79fc851e8
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-21 16:45:18 +0100

    Adds remaining auth cmd tests for auth state

--- lib_tests/State.ml
+++ lib_tests/State.ml
@@ -41,15 +41,6 @@ module TestOkState = BackingStoreState (ConstBanner) (NoopOkStore)
 
 module TestErrState = BackingStoreState (ConstBanner) (NoopErrStore)
 
-let f_auth_none_user_ok_mailbox_reply switch () =
-  Lwt_switch.add_hook (Some switch) (fun () -> Lwt.return ());
-  let cmd = User mailbox in
-  TestOkState.start ""
-  >>= fun s -> TestOkState.f s cmd
-  >|= fun (_,r) ->
-    Alcotest.(check string) "Checking reply."
-      "+OK 123" (Pop3.Reply.string_of_t r)
-
 let f_auth_none_apop_ok_mailbox_reply switch () =
   Lwt_switch.add_hook (Some switch) (fun () -> Lwt.return ());
   let cmd = Apop (mailbox, digest) in
@@ -59,14 +50,21 @@ let f_auth_none_apop_ok_mailbox_reply switch () =
     Alcotest.(check string) "Checking reply."
       "+OK 123" (Pop3.Reply.string_of_t r)
 
-let f_auth_some_pass_ok_mailbox_reply switch () =
+let f_auth_none_pass_err_reply switch () =
   Lwt_switch.add_hook (Some switch) (fun () -> Lwt.return ());
-  let cmd_user = User mailbox in
-  let cmd_pass = Pass secret in
+  let cmd = Pass secret in
   TestOkState.start ""
-  >>= fun s -> TestOkState.f s cmd_user
-  >>= fun (s',_) -> TestOkState.f s' cmd_pass
-  >|= fun (_ ,r) ->
+  >>= fun s -> TestOkState.f s cmd
+  >|= fun (_,r) ->
+    Alcotest.(check string) "Checking reply."
+      "-ERR" (Pop3.Reply.string_of_t r)
+
+let f_auth_none_user_ok_mailbox_reply switch () =
+  Lwt_switch.add_hook (Some switch) (fun () -> Lwt.return ());
+  let cmd = User mailbox in
+  TestOkState.start ""
+  >>= fun s -> TestOkState.f s cmd
+  >|= fun (_,r) ->
     Alcotest.(check string) "Checking reply."
       "+OK 123" (Pop3.Reply.string_of_t r)
 
@@ -81,9 +79,32 @@ let f_auth_some_apop_err_reply switch () =
     Alcotest.(check string) "Checking reply."
       "-ERR" (Pop3.Reply.string_of_t r)
 
+let f_auth_some_pass_ok_mailbox_reply switch () =
+  Lwt_switch.add_hook (Some switch) (fun () -> Lwt.return ());
+  let cmd_user = User mailbox in
+  let cmd_pass = Pass secret in
+  TestOkState.start ""
+  >>= fun s -> TestOkState.f s cmd_user
+  >>= fun (s',_) -> TestOkState.f s' cmd_pass
+  >|= fun (_ ,r) ->
+    Alcotest.(check string) "Checking reply."
+      "+OK 123" (Pop3.Reply.string_of_t r)
+
+let f_auth_some_user_err_reply switch () =
+  Lwt_switch.add_hook (Some switch) (fun () -> Lwt.return ());
+  let cmd_user = User mailbox in
+  TestOkState.start ""
+  >>= fun s -> TestOkState.f s cmd_user
+  >>= fun (s',_) -> TestOkState.f s' cmd_user
+  >|= fun (_ ,r) ->
+    Alcotest.(check string) "Checking reply."
+      "-ERR" (Pop3.Reply.string_of_t r)
+
 let unit_tests = [
-  Alcotest_lwt.test_case "Check reply from valid USER command." `Quick f_auth_none_user_ok_mailbox_reply;
   Alcotest_lwt.test_case "Check reply from valid APOP command." `Quick f_auth_none_user_ok_mailbox_reply;
-  Alcotest_lwt.test_case "Check reply from valid PASS command." `Quick f_auth_some_pass_ok_mailbox_reply;
+  Alcotest_lwt.test_case "Check reply from invalid PASS command (not following a USER command)." `Quick f_auth_none_pass_err_reply;
+  Alcotest_lwt.test_case "Check reply from valid USER command." `Quick f_auth_none_user_ok_mailbox_reply;
   Alcotest_lwt.test_case "Check reply from invalid APOP command (following a USER command)." `Quick f_auth_some_apop_err_reply;
+  Alcotest_lwt.test_case "Check reply from valid PASS command." `Quick f_auth_some_pass_ok_mailbox_reply;
+  Alcotest_lwt.test_case "Check reply from invalid USER command (following a USER command)." `Quick f_auth_some_user_err_reply;
 ]

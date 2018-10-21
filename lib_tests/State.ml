0cf9165803c6aa670028298d704501ba022ce7c6
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-21 16:39:26 +0100

    Adds initial auth state tests

--- lib_tests/State.ml
+++ lib_tests/State.ml
@@ -50,6 +50,40 @@ let f_auth_none_user_ok_mailbox_reply switch () =
     Alcotest.(check string) "Checking reply."
       "+OK 123" (Pop3.Reply.string_of_t r)
 
+let f_auth_none_apop_ok_mailbox_reply switch () =
+  Lwt_switch.add_hook (Some switch) (fun () -> Lwt.return ());
+  let cmd = Apop (mailbox, digest) in
+  TestOkState.start ""
+  >>= fun s -> TestOkState.f s cmd
+  >|= fun (_,r) ->
+    Alcotest.(check string) "Checking reply."
+      "+OK 123" (Pop3.Reply.string_of_t r)
+
+let f_auth_some_pass_ok_mailbox_reply switch () =
+  Lwt_switch.add_hook (Some switch) (fun () -> Lwt.return ());
+  let cmd_user = User mailbox in
+  let cmd_pass = Pass secret in
+  TestOkState.start ""
+  >>= fun s -> TestOkState.f s cmd_user
+  >>= fun (s',_) -> TestOkState.f s' cmd_pass
+  >|= fun (_ ,r) ->
+    Alcotest.(check string) "Checking reply."
+      "+OK 123" (Pop3.Reply.string_of_t r)
+
+let f_auth_some_apop_err_reply switch () =
+  Lwt_switch.add_hook (Some switch) (fun () -> Lwt.return ());
+  let cmd_user = User mailbox in
+  let cmd_apop = Apop (mailbox, digest) in
+  TestOkState.start ""
+  >>= fun s -> TestOkState.f s cmd_user
+  >>= fun (s',_) -> TestOkState.f s' cmd_apop
+  >|= fun (_ ,r) ->
+    Alcotest.(check string) "Checking reply."
+      "-ERR" (Pop3.Reply.string_of_t r)
+
 let unit_tests = [
-  Alcotest_lwt.test_case "a" `Quick f_auth_none_user_ok_mailbox_reply
+  Alcotest_lwt.test_case "Check reply from valid USER command." `Quick f_auth_none_user_ok_mailbox_reply;
+  Alcotest_lwt.test_case "Check reply from valid APOP command." `Quick f_auth_none_user_ok_mailbox_reply;
+  Alcotest_lwt.test_case "Check reply from valid PASS command." `Quick f_auth_some_pass_ok_mailbox_reply;
+  Alcotest_lwt.test_case "Check reply from invalid APOP command (following a USER command)." `Quick f_auth_some_apop_err_reply;
 ]

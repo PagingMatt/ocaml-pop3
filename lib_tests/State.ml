bcab6fa9472854d8c8b45856f881cd74ee1d7c35
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-21 16:50:09 +0100

    Extracts helpers module

--- lib_tests/State.ml
+++ lib_tests/State.ml
@@ -3,46 +3,50 @@ open Pop3.Command
 open Pop3.State
 open Pop3.Store
 
-let digest = "<1514764801.0@>abc"
+module Helpers = struct
+  let digest = "<1514764801.0@>abc"
 
-let mailbox = "123"
+  let mailbox = "123"
 
-let secret = "abc"
+  let secret = "abc"
 
-module ConstBanner : Banner = struct
-  let time () = Unix.gmtime 1514764801.0
+  module ConstBanner : Banner = struct
+    let time () = Unix.gmtime 1514764801.0
+  end
+
+  module NoopOkStore : Store = struct
+    type t = unit
+
+    let init _p = Lwt.return ()
+
+    let secret_of_mailbox _s _m =
+      Lwt.return (Some secret)
+
+    let apop_of_mailbox _s _t _h _m =
+      Lwt.return (Some digest)
+  end
+
+  module NoopErrStore : Store = struct
+    type t = unit
+
+    let init _p = Lwt.return ()
+
+    let secret_of_mailbox _s _m =
+      Lwt.return (Some secret)
+
+    let apop_of_mailbox _s _t _h _m =
+      Lwt.return (Some digest)
+  end
+
+  module TestOkState = BackingStoreState (ConstBanner) (NoopOkStore)
+
+  module TestErrState = BackingStoreState (ConstBanner) (NoopErrStore)
 end
-
-module NoopOkStore : Store = struct
-  type t = unit
-
-  let init _p = Lwt.return ()
-
-  let secret_of_mailbox _s _m =
-    Lwt.return (Some secret)
-
-  let apop_of_mailbox _s _t _h _m =
-    Lwt.return (Some digest)
-end
-
-module NoopErrStore : Store = struct
-  type t = unit
-
-  let init _p = Lwt.return ()
-
-  let secret_of_mailbox _s _m =
-    Lwt.return (Some secret)
-
-  let apop_of_mailbox _s _t _h _m =
-    Lwt.return (Some digest)
-end
-
-module TestOkState = BackingStoreState (ConstBanner) (NoopOkStore)
-
-module TestErrState = BackingStoreState (ConstBanner) (NoopErrStore)
 
 module Authorization = struct
   module None = struct
+    open Helpers
+
     let f_auth_none_apop_ok_mailbox_reply switch () =
       Lwt_switch.add_hook (Some switch) (fun () -> Lwt.return ());
       let cmd = Apop (mailbox, digest) in
@@ -78,6 +82,8 @@ module Authorization = struct
   end
 
   module Some = struct
+    open Helpers
+
     let f_auth_some_apop_err_reply switch () =
       Lwt_switch.add_hook (Some switch) (fun () -> Lwt.return ());
       let cmd_user = User mailbox in

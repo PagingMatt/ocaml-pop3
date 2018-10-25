1febbdf9d932310ec7eb5bc50464d984d18391e2
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-25 21:41:10 +0100

    Handle End_of_file exn

--- lib/Server.ml
+++ lib/Server.ml
@@ -9,17 +9,21 @@ module Server (S : State) : sig
 end = struct
   let rec iter_state input_channel output_channel state =
     if S.terminated state then Lwt.return () else
-    Lwt_io.read_line input_channel
-    >|= Command.t_of_string_opt
-    >>= fun cmd_opt ->
-      (match cmd_opt with
-      | None -> Lwt.return (state, Reply.err None)
-      | Some cmd -> S.f state cmd)
-    >>= (fun (state', reply) ->
-      reply
-      |> Reply.lines_of_t
-      |> Lwt_list.iter_s (Lwt_io.write_line output_channel)
-      >>= fun () -> iter_state input_channel output_channel state')
+    try
+      Lwt_io.read_line input_channel
+      >|= Command.t_of_string_opt
+      >>= fun cmd_opt ->
+        (match cmd_opt with
+        | None -> Lwt.return (state, Reply.err None)
+        | Some cmd -> S.f state cmd)
+      >>= (fun (state', reply) ->
+        reply
+        |> Reply.lines_of_t
+        |> Lwt_list.iter_s (Lwt_io.write_line output_channel)
+        >>= fun () -> iter_state input_channel output_channel state')
+    with
+    (* Client has disconnected. *)
+    | End_of_file -> Lwt.return ()
 
   let callback hostname maildrop _flow input_channel output_channel =
     S.start hostname maildrop

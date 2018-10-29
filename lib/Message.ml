d09570de8b21983dbfb612f87d89261dd604fa6f
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-29 16:31:39 +0000

    Adds message parsing

--- lib/Message.ml
+++ lib/Message.ml
@@ -5,8 +5,27 @@ module type MessageParser = sig
 end
 
 module JsonMessageParser : MessageParser = struct
-  let lines_of_string _msg =
-    None
+  open Yojson.Basic
+
+  let rec fold_lines acc ls =
+    match acc with
+    | None       -> None
+    | Some lines ->
+      match ls with
+      | (`String line)::ls' ->
+        fold_lines (Some (line::lines)) ls'
+      | _ -> None
+
+  let lines_of_string msg =
+    match (from_string msg) with
+    | `List lines ->
+      (* Top-level JSON structure should be a list. *)
+      (match fold_lines (Some []) lines with
+      | Some ls -> Some (List.rev ls)
+      | None    -> None)
+    | _ ->
+      (* Any other top-level JSON structure is a failure. *)
+      None
 
   let uid_of_string _msg =
     None

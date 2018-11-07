8f32ae670cdbd08bd4615feeeeed7294a94bd936
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-11-07 23:17:12 +0000

    Pulls Irmin store back to pop3-server

--- lib/pop3_server/Store.ml
+++ lib/pop3_server/Store.ml
@@ -1,3 +1,5 @@
+open Message
+
 module type Store = sig
   type t
 
@@ -14,4 +16,73 @@ module type Store = sig
   val lines_of_message : t -> string -> int -> string list option Lwt.t
 
   val uid_of_message : t -> string -> int -> string option Lwt.t
+end
+
+module type IrminStringKv =
+  Irmin.KV with type contents = Irmin.Contents.String.t
+
+module IrminStore (S : IrminStringKv) (P : MessageParser) : Store = struct
+  open Lwt.Infix
+
+  type t = S.t
+
+  let init p =
+    Irmin_git.config ~bare:false p
+    |> S.Repo.v
+    >>= S.master
+
+  let secret_of_mailbox s m =
+    S.find s ["secrets"; m]
+
+  let apop_of_mailbox s t h m =
+    secret_of_mailbox s m
+    >|= fun secret_option ->
+      match secret_option with
+      | None -> None
+      | Some secret ->
+        let ft, _ = Unix.mktime t in
+        Printf.sprintf "<%f@%s>%s" ft h secret
+        |> Digest.string
+        |> fun digest -> Some digest
+
+  let message_list_of_mailbox s m =
+    S.list s ["mailboxes"; m]
+    >|= List.filter
+      (fun (num, ls) ->
+        match num |> int_of_string_opt with
+        | Some _ ->
+          (match ls with
+          | `Contents -> true
+          | `Node     -> false)
+        | None   -> false)
+    >|= List.map
+      (fun (num, _) -> int_of_string num)
+
+  let read_message s m i =
+    string_of_int i
+    |> fun msg -> S.find s ["mailboxes"; m; msg]
+
+  let lines_of_message s m i =
+    read_message s m i
+    >|= fun contents ->
+      match contents with
+      | None -> None
+      | Some s -> P.lines_of_string s
+
+  let octets_of_message s m i =
+    lines_of_message s m i
+    >|= fun ls_opt ->
+      match ls_opt with
+      | None    -> None
+      | Some ls -> Some (ls
+        |> String.concat "\r\n"
+        |> Bytes.of_string
+        |> Bytes.length)
+
+  let uid_of_message s m i =
+    read_message s m i
+    >|= fun contents ->
+      match contents with
+      | None -> None
+      | Some s -> P.uid_of_string s
 end
--- lib/pop3_server_unix/Store.mli
+++ lib/pop3_server_unix/Store.mli
@@ -1,8 +1,5 @@
 (** Store of mailbox secrets and maildrop content. *)
 
-open Pop3_server.Message
 open Pop3_server.Store
 
-(** The [IrminStore] module is an implementation of the [Store] signature which
-    uses Irmin for the underlying store. *)
-module IrminStore (P : MessageParser) : Store
+module IrminUnixStore : Store
